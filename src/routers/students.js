4247020e48777d8dee56228661bab90dbacf100f
Fakhri <mightydhiyamatra@yahoo.co.id>
2022-01-15 14:19:02 +0700

    add get profile

--- src/routers/students.js
+++ src/routers/students.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const studentController = require("../controllers/students");
 
+const db = require("../config/db");
+
 const studentRouter = express.Router();
 
 studentRouter.get("/", studentController.getStudentsWithOrder);
+
+studentRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db.query("SELECT * FROM users WHERE ?", [{ id }], (err, result) => {
+    if (err) return res.status(500).json({ err });
+    res.status(200).json({ result });
+  });
+});
 
 module.exports = studentRouter;
